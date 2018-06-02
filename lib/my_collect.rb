a4d09623fe9d97934115a0283834f3ef44672973
Peter Kim <peterckim96@gmail.com>
2018-06-02 02:34:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+  count = 0
+  output = []
+  while count < array.length
+    output << yield(array[count])
+    count += 1
+  end
+  output
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+# collection = ["ruby", "javascript", "python", "objective-c"]
+
+# my_collect(collection) do |lang|
+#   lang.upcase
+# end
