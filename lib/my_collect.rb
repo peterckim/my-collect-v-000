eda940bb7d011b1de3fd136793c7cd0843f83412
Peter Kim <peterckim96@gmail.com>
2018-06-02 02:33:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ["ruby", "javascript", "python", "objective-c"]
+
+my_collect(collection) do |lang|
+  lang.upcase
+end
